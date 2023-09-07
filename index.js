baee7616e2c82044e40c2bca6b06a913ecd220a7
fcroux31 <nobrainnogaincoaching@gmail.com>
2023-09-07 16:57:43 +0200

    version working local

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const { prefix, token } = require('./config.json');
 
 const client = new Discord.Client({ intents: [
     Discord.GatewayIntentBits.Guilds,
-    Discord.GatewayIntentBits.GuildMessages
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.MessageContent,
+    Discord.GatewayIntentBits.DirectMessages
 ]})
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
 
 let phrases = [];
 
@@ -13,24 +19,39 @@ try {
     const phrasesData = fs.readFileSync('./phrases.json');
     phrases = JSON.parse(phrasesData);
 } catch (err) {
-    console.error('Erreur lors de la lecture du fichier phrases.json', err);
+    console.error('Erreur lecture phrases.json', err);
 }
 
-client.once('ready', () => {
-    console.log('Ready!');
-});
-
-client.on('message', (message) => {
-if (message.content.includes('=alexia')) {
-    console.log('=alexia détecté');
-    const randomIndex = Math.floor(Math.random() * phrases.length);
-    const randomPhrase = phrases[randomIndex];
-    if (randomPhrase) {
-    message.channel.send(randomPhrase);
-    } else {
-    message.channel.send('Aucune phrase trouvée dans la base de données.');
+client.on('messageCreate', async message => {
+    if (!message.content.startsWith(prefix)) {
+        return;
+    }    
+    if (message.content.startsWith(prefix + "alexia")){
+        const randomIndex = Math.floor(Math.random() * phrases.length);
+        const randomPhrase = phrases[randomIndex];
+        if (randomPhrase) {
+            message.channel.send(randomPhrase);
+            return;
+        }   
     }
-}
+    else {
+        message.channel.send("Perdu...tu peux ressayer avec le bon préfixe ainsi que le prénom de la boomeuse ultime !");
+        return;
+    }
 });
+
+client.on('messageCreate', async message => {
+    if (message.content.includes("chameau") && message.author.id == "1097466283764756570") {
+    let url = 'https://media.tenor.com/_cEumE-qCeYAAAAC/camel-race.gif';
+    message.channel.send(url);
+    return;
+}});
+
+client.on('messageCreate', async message => {
+    if (message.content.includes("sophie")) {
+    let url2 = 'https://media.tenor.com/CAEyorifT40AAAAd/big-trouble-if-i-speak.gif';
+    message.channel.send(url2);
+    return;
+}});
 
 client.login(token);
